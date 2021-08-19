ce468aedc74f6f202f855194b6b692839586471a
Sara Pearce <sarapearce3.14@gmail.com>
2021-08-19 13:28:14 -0500

    initial commit from Cypress code

---
+++ cypress/plugins/index.js
@@ -0,0 +1,7 @@
+module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config)
+
+  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+
+  return config
+}
